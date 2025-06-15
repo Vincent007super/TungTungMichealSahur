9d68e3da2f149dd0c08fbd420e31af0f5d3628c7
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2025-06-15 16:00:35 +0200

    Added Michiel
    - Added Michiel sprite
    - Added michieal move logic

--- game.js
+++ game.js
@@ -10,4 +10,20 @@ async function run() {
 
     document.body.appendChild(app.canvas);
 
+    const michielTexture = await Assets.load('assets/sprites/michiel.png');
+
+    const michiel = new Sprite(michielTexture);
+    michiel.anchor.set(0.5);
+    michiel.scale.set(0.3); // Make sure he's visible
+    michiel.x = app.screen.width / 2;
+    michiel.y = 0;
+    michiel.vy = 0; // vertical speed
+    app.stage.addChild(michiel);
+    const keys = {};
+    window.addEventListener("keydown", e => keys[e.code] = true);
+    window.addEventListener("keyup", e => keys[e.code] = false);
+    app.ticker.add(() => {
+        // Horizontal movement
+        if (keys["ArrowLeft"]) michiel.x -= 5;
+        if (keys["ArrowRight"]) michiel.x += 5;
 run();
